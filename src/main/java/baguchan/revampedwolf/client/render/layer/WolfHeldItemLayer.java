e068791ec4a6500716c71858e83d94c9051fc807
baguchan <shutux500@gmail.com>
2023-09-14 15:48:46 +0900

    update to neo forged

--- src/main/java/baguchan/revampedwolf/client/render/layer/WolfHeldItemLayer.java
+++ src/main/java/baguchan/revampedwolf/client/render/layer/WolfHeldItemLayer.java
@@ -2,9 +2,9 @@ package baguchan.revampedwolf.client.render.layer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.HeadedModel;
 import net.minecraft.client.model.WolfModel;
+import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
@@ -17,32 +17,27 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class WolfHeldItemLayer extends RenderLayer<Wolf, WolfModel<Wolf>> {
-	public WolfHeldItemLayer(RenderLayerParent<Wolf, WolfModel<Wolf>> p_116994_) {
+	private final ItemInHandRenderer itemInHandRenderer;
+
+	public WolfHeldItemLayer(RenderLayerParent<Wolf, WolfModel<Wolf>> p_116994_, ItemInHandRenderer p_234839_) {
 		super(p_116994_);
+		this.itemInHandRenderer = p_234839_;
 	}
 
-	public void render(PoseStack p_117007_, MultiBufferSource p_117008_, int p_117009_, Wolf p_117010_, float p_117011_, float p_117012_, float p_117013_, float p_117014_, float p_117015_, float p_117016_) {
-		if (this.getParentModel() instanceof HeadedModel) {
+	public void render(PoseStack poseStack, MultiBufferSource p_117008_, int p_117009_, Wolf p_117010_, float p_117011_, float p_117012_, float p_117013_, float p_117014_, float p_117015_, float p_117016_) {
+		if (this.getParentModel() instanceof HeadedModel headedModel) {
 			boolean flag = p_117010_.isSleeping();
 			boolean flag1 = p_117010_.isBaby();
-			p_117007_.pushPose();
-			if (flag1) {
-				float f = 0.75F;
-				p_117007_.scale(0.75F, 0.75F, 0.75F);
-				p_117007_.translate(0.0D, 0.65D, 0.0D);
-			}
-
-			p_117007_.translate((double) (((HeadedModel) this.getParentModel()).getHead().x / 16.0F), (double) (((HeadedModel) this.getParentModel()).getHead().y / 16.0F), (double) (((HeadedModel) this.getParentModel()).getHead().z / 16.0F));
-			float f1 = p_117010_.getHeadRollAngle(p_117013_);
-			p_117007_.mulPose(Axis.ZP.rotation(f1));
-			p_117007_.mulPose(Axis.YP.rotationDegrees(p_117015_));
-			p_117007_.mulPose(Axis.XP.rotationDegrees(p_117016_));
-			p_117007_.translate(0.059D, 0.15D, -0.42D);
-			p_117007_.mulPose(Axis.XP.rotationDegrees(90.0F));
-
-			ItemStack itemstack = p_117010_.getItemBySlot(EquipmentSlot.MAINHAND);
-			Minecraft.getInstance().getEntityRenderDispatcher().getItemInHandRenderer().renderItem(p_117010_, itemstack, ItemDisplayContext.GROUND, false, p_117007_, p_117008_, p_117009_);
-			p_117007_.popPose();
+			poseStack.pushPose();
+			headedModel.getHead().translateAndRotate(poseStack);
+			float scale = flag1 ? 0.75F : 1.0F;
+			poseStack.translate(0, 0, -0.5F * scale);
+			poseStack.mulPose(Axis.YP.rotationDegrees(p_117015_));
+			poseStack.mulPose(Axis.XP.rotationDegrees(-p_117016_));
+			poseStack.mulPose(Axis.XP.rotationDegrees(90.0f));
+			ItemStack itemStack = p_117010_.getItemBySlot(EquipmentSlot.MAINHAND);
+			this.itemInHandRenderer.renderItem(p_117010_, itemStack, ItemDisplayContext.GROUND, false, poseStack, p_117008_, p_117009_);
+			poseStack.popPose();
 		}
 	}
 }
