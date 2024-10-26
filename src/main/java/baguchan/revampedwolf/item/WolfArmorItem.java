4c4d25f6dbb778f858229c8a6b02fdefc9ae7867
baguchan <shutux500@gmail.com>
2024-10-26 23:36:05 +0900

    1.21.3-14.0.0 enchantable valve fix

--- src/main/java/baguchan/revampedwolf/item/WolfArmorItem.java
+++ src/main/java/baguchan/revampedwolf/item/WolfArmorItem.java
@@ -5,6 +5,6 @@ import net.minecraft.world.item.equipment.ArmorMaterial;
 
 public class WolfArmorItem extends AnimalArmorItem {
     public WolfArmorItem(ArmorMaterial p_371643_, BodyType p_324315_, Properties p_316341_) {
-        super(p_371643_, p_324315_, p_316341_);
+        super(p_371643_, p_324315_, p_316341_.enchantable(p_371643_.enchantmentValue()));
     }
 }
